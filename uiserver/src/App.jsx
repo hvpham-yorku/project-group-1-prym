485b70af4b5787fb3a11c89a8d7a43405143dcae
katehop210 <katehop@my.yorku.ca>
2026-02-05 08:44:49 -0500

    Renamed files + created link from listings page to a single listing

--- uiserver/src/App.jsx
+++ uiserver/src/App.jsx
@@ -5,7 +5,8 @@ import BuyerSignup from './pages/auth/BuyerSignup';
 import SellerSignup from './pages/auth/SellerSignup';
 import BuyerDashboard from './pages/BuyerDashboard';
 import SellerDashboard from './pages/SellerDashboard';
-import FarmListings from './pages/FarmListings';
+import FarmListingsPage from './pages/FarmListingsPage';
+import FarmListing from './pages/FarmListing';
 
 function ProtectedRoute({ children, allowedRole }) {
     const { user } = useAuth();
@@ -45,7 +46,8 @@ function AppRoutes() {
             <Route path="/" element={<Navigate to="/login" />} />
             <Route path="*" element={<Navigate to="/login" />} />
 			
-			<Route path="/farmlistings" element={<FarmListings />} />
+			<Route path="/farmlistingspage" element={<FarmListingsPage />} />
+			<Route path="/farmlistingspage/farmlisting" element={<FarmListing />} />
         </Routes>
     );
 }
---
+++ uiserver/src/pages/FarmListingsPage.jsx
@@ -0,0 +1,13 @@
+function FarmListingsPage(){
+	return(
+		
+		
+		<div>
+			<h1>Farm Listings</h1>
+			<p>This page is empty right now but it will have something great soon...</p>
+			<a href='/farmlistingspage/farmlisting'><button>Farm Listing</button></a>
+		</div>
+	);
+}
+
+export default FarmListingsPage;
