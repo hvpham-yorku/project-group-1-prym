14267111b542f93166dfa7166077c2630625f269
katehop210 <katehop@my.yorku.ca>
2026-02-05 11:01:47 -0500

    made the (barebones) listings look better

--- uiserver/src/pages/FarmListingsPage.jsx
+++ uiserver/src/pages/FarmListingsPage.jsx
@@ -1,13 +1,39 @@
 function FarmListingsPage(){
 	return(
-		
-		
 		<div>
-			<h1>Farm Listings</h1>
-			<p>This page is empty right now but it will have something great soon...</p>
-			<a href='/farmlistingspage/farmlisting'><button>Farm Listing</button></a>
+			<h1 style={styles.header}>Farm Listings</h1>
+			<div style={styles.container}>
+				<a href='/farmlistingspage/farmlisting'><button style={styles.button}>Farm Listing 1</button></a>
+				<a href='/farmlistingspage/farmlisting'><button style={styles.button}>Farm Listing 2</button></a>
+				<a href='/farmlistingspage/farmlisting'><button style={styles.button}>Farm Listing 3</button></a>
+				<a href='/farmlistingspage/farmlisting'><button style={styles.button}>Farm Listing 4</button></a>
+				<a href='/farmlistingspage/farmlisting'><button style={styles.button}>Farm Listing 5</button></a>
+			</div>
 		</div>
 	);
 }
+
+
+
+const styles = {
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	button: {
+		backgroundColor: '#04AA6D',
+		color: 'white',
+		fontSize: 100,
+		padding: 12,
+		width: 10000,
+		border: 'none',
+		borderRadius: 20,
+		cursor: 'pointer',	
+	},
+	header: {
+		backgroundColor: 'green',
+	},
+};
 
 export default FarmListingsPage;
