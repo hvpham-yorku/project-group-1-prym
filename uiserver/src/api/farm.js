e2993b6afd854ab4c93e162c5b77bb997b2054a8
katehop210 <katehop@my.yorku.ca>
2026-03-05 22:41:41 -0500

    changes to the api calls

--- uiserver/src/api/farm.js
+++ uiserver/src/api/farm.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 
-export function getAllFarms(){
+/*export function getAllFarms(){
 	const [farms, setFarms] = useState([]);
 		
 	useEffect(() => {
@@ -10,6 +10,26 @@ export function getAllFarms(){
 		.catch(err => console.error("Failed to load farms", err))
 	}, []);
 		
+	
+	
+	return farms;
+}*/
+
+
+export function getAllFarms(){
+	const [farms, setFarms] = useState([]);
+	
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch('/api/seller/all');
+			const data = await response.json();
+			setFarms(data);
+		};
+		fetchData().catch(console.error);
+	}, [])
+	
+	console.log(Array.isArray(farms));
+	
 	return farms;
 }
 
