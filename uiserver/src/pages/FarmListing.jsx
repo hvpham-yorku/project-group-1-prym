b6f90540251138dac2549523b382c17853c71aba
Sufyan Serdah <serdahsufyan@gmail.com>
2026-03-26 20:14:49 -0400

    one of the fixes was broken

--- uiserver/src/pages/FarmListing.jsx
+++ uiserver/src/pages/FarmListing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import { getFarm, saveFarm, getSavedFarms, removeSavedFarm, getCowTypes } from '../api/farm';
 
 function FarmListing(){
